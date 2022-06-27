69d53a890be9892ef947534f01c7a91e527d4318
raife-levy <91220117+raife-levy@users.noreply.github.com>
2022-06-27 14:07:26 -0400

    Update createcsv.py
    added line 9
    added comment on line 9

--- piZero/CSV/createcsv.py
+++ piZero/CSV/createcsv.py
@@ -6,7 +6,7 @@ while True:
     try:
         readedText = ser.readline() #readline gets entire line of data until the end of file
         txtArr = readedText.split() #splits string into array
-        txtArr = [str(x)[2:-1] for x in txtArr]
+        txtArr = [str(x)[2:-1] for x in txtArr] #gets rid of b'' string formatting
         with open('results.csv', 'a') as file: #opens the csv file the data will be added to. the 'a' indicates append,(cont)
             writer = csv.writer(file) #(cont) which adds the data to the end of the csv instead of creating a new one
             writer.writerow(txtArr) #write new row using the raw Pico data in txtArr
