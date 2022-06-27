ddbed9df231fdf0c0a923a53575628ac0059e107
raife-levy <91220117+raife-levy@users.noreply.github.com>
2022-06-27 13:28:35 -0400

    Update createcsv.py

--- piZero/CSV/createcsv.py
+++ piZero/CSV/createcsv.py
@@ -6,14 +6,9 @@ while True:
     try:
         readedText = ser.readline() #readline gets entire line of data until the end of file
         txtArr = readedText.split() #splits string into array
-        with open('results.csv', 'a') as file:
-            writer = csv.writer(file)
-            writer.writerow(txtArr)
+        with open('results.csv', 'a') as file: #opens the csv file the data will be added to. the 'a' indicates append,(cont)
+            writer = csv.writer(file) #(cont) which adds the data to the end of the csv instead of creating a new one
+            writer.writerow(txtArr) #write new row using the raw Pico data in txtArr
             time.sleep(2)
     except Exception as e:
-        print("Exception: ", e)
-                    
-
-            
-  
-
+        print("Exception: ", e) 
